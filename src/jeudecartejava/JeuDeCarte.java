919767347c3aaf698d5f15866a5b358159ff369c
Quentin Gueguen <quentingueguen@yahoo.fr>
2022-11-23 15:30:14 +0100

    Ajout d'un input pour le nom

--- src/jeudecartejava/JeuDeCarte.java
+++ src/jeudecartejava/JeuDeCarte.java
@@ -1,14 +1,38 @@
 package jeudecartejava;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class JeuDeCarte {
+	private static Scanner nom;
+
 	public static void main(String[] args) {
-		Joueur j1 = new Joueur("Quentin");
-		for(int i = 0; i < j1.deck.length; i++){
-			Carte.print(j1.deck[i]);
+		Map<String, Joueur> listeDesJoueurs = new HashMap<String, Joueur>();
+		int nombreDeJoueurs = 0;
+
+		while (nombreDeJoueurs != 2) {
+			String nomNouveauJoueur = initialisation();
+			Joueur nouveauJoueur = new Joueur(nomNouveauJoueur);
+			listeDesJoueurs.put(nomNouveauJoueur, nouveauJoueur);
+			nombreDeJoueurs += 1;
 		}
-		
+		for (Map.Entry<String, Joueur> clef : listeDesJoueurs.entrySet()) {
+			Joueur clefJoueur = clef.getValue();
+			Carte.print(clefJoueur.deck[1]);
+		}
+
+	}
+
+	private static String initialisation() {
+
+		nom = new Scanner(System.in);
+		System.out.print("Entrez le nom du joueur \n");
+
+		String nomJoueur = nom.nextLine();
+		// nom.close();
+
+		return nomJoueur;
+
 	}
 }
-
-
-
